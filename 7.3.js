505f5db9c01e9966063de88f048c5a780a63e402
sabzj <zjsabbah@gmail.com>
2023-11-02 07:33:02 +0200

    ex.7.3 answer

---
+++ 7.3.js
@@ -0,0 +1,7 @@
+//  create a new array
+//  Use slice that will remove all the elements from the array
+//  new array includes => “Orange” and “Lemon”
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let newFruits = fruits.slice(1, 3);
+console.log(newFruits);
