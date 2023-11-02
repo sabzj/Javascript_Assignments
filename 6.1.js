48b87a599cb98799a54608fa5e5f19059b0cef3b
sabzj <zjsabbah@gmail.com>
2023-11-02 08:09:52 +0200

    simple_for_loop

---
+++ 6.1.js
@@ -0,0 +1,14 @@
+// 6.1 There are elections in your country! In a small town, there are
+// only 50 voters. Use a for loop to simulate the 50 people voting,
+// by logging a string like this to the console (for numbers 1 to 50):
+// 'Voter number 1 is currently voting
+//  50 voters
+
+//  Voters Numbers 1-50
+// Make for loop from 1-50
+// Output “string” == voter number () is currently voting
+
+let voter = 50;
+for (let i = 1; i < 50; i++) {
+  console.log(`voter number ${i} is currently voting`);
+}
