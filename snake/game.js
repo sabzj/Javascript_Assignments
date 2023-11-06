816a0d99c6e66bc75c97bc2fbe30581a11f01fda
sabzj <zjsabbah@gmail.com>
2023-11-06 21:58:36 +0200

    Coding the YouTube video of snake game

---
+++ snake/game.js
@@ -0,0 +1,55 @@
+import {
+  update as updatesnake,
+  draw as drawsnake,
+  SNAKE_SPEED, getSnakeHead, snakeIntersection
+} from "./snake.js";
+
+import {update as updateFood, draw as drawFood} from `./food.js`
+import {outSideGrid} from ' ./grid.js'
+
+
+let lastRenderTime = 0;
+let gameOver = false
+const gameBoard = document.getElementById("game board");
+
+function main(currentTime) {
+    if (gameOver) {
+       if (confirm ('You Lost. Press Ok To restart. ')); {
+        window.location = '/'
+       }
+       return
+}  
+
+window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  //   console.log(currentTime);
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+window.requestAnimationFrame(main);
+
+function update() {
+  updatesnake();
+  updateFood()
+  checkDeath()
+}
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawsnake(gameBoard);
+  drawFood (gameBoard)
+}
+
+function checkDeath () {
+    gameOver = outSideGR(getSnakeHed) || snakeIntersection()
+}
+
+export function outSideGrid (position) {
+    return (
+        position.x < 1 || position.x > GRID_SIZE ||
+        position.Y < 1 || position.Y > GRID_SIZE
+    )
+}
